3c59d24169c1b827ace2e10039bd556c9fc04dad
NitishTadikonda <nitishtadikonda@gmail.com>
2017-09-14 22:54:44 +0530

    Programs

--- Tasks/Notes using Dict.py
+++ Tasks/Notes using Dict.py
@@ -1,26 +1,29 @@
 class Notes:
     def __init__(self):
         self.Notes = {}
+        
+    def addtitle(self,title, content):
+        self.Notes[title] = self.Notes.get(title, content)
+        
     def process(self):
       while True:
         n = int(input("enter the number 1  for writting content and 2 for retriving the  data"))
         if n == 1:
-            def addtitle(title, content):
-                self.Notes[title] = self.Notes.get(title, content)
-            addtitle(raw_input("enter the title :"),raw_input("enter the content:"))
+            self.addtitle(raw_input("enter the title :"),raw_input("enter the content:"))
+            
         elif n==2:
           if self.Notes == {}:
-              def addtitle(title, content):
-                self.Notes[title] = self.Notes.get(title, content)
-              addtitle(raw_input("enter the title :"),raw_input("enter the content:"))
-              print (self.Notes)   # it prints the dictionay
+              self.addtitle(raw_input("enter the title :"),raw_input("enter the content:"))
+              #print (self.Notes)it prints the dictionay
           else:
+              
             for a in enumerate(self.Notes,1): #it  get the index for  keys-valuespairs
                 print (a)
+                
             self.Notes.items()
             d = [(k, v) for k, v in self.Notes.items()]  # converts into list
             i=int(input("enter the number for retriving the data"))
-            print (d[i+1])
+            print (d[i-1])
 
 o = Notes()
 o.process()
--- Tasks/Notes using list.py
+++ Tasks/Notes using list.py
@@ -3,26 +3,24 @@ class Notes:
         self.title = []
         self.content = []
         
+    def addtitle(self,title,content):
+        self.title.append(title)
+        self.content.append(content)
+        
     def process(self):
         while True:
-            n = int(input("enter the number 1  for writting content and 2 for retriving the  data"))
+            n = int(input("Enter the number 1  for writting content and 2 for retriving the  data"))
             if n == 1:
-                def addtitle(title,content):
-                    self.title.append(title) 
-                    self.content.append(content)
-                addtitle(raw_input("enter title"),raw_input("enter content"))
+                self.addtitle(input("Enter Title:"),input("Enter Content:"))
+        
             elif n==2:
                 if self.title == [] or self.content == []:
-                    def addtitle(title,content):
-                        self.title.append(title) 
-                        self.content.append(content)
-                    addtitle(raw_input("enter title"),raw_input("enter content"))
-                    
+                    self.addtitle(input("Enter Title:"),input("Enter Content:"))                    
                 else:
                     c = enumerate(self.title,1)
                     for i in c:
                         print(i)
-                    i=int(input("enter the number for retriving the data"))
+                    i=int(input("Enter the number for retriving the data"))
                     #d = map(lambda x,y:(x,y),self.title,self.content)to display in list like as zip function
                     print("Title:%s\nContent:%s"%(self.title[i-1],self.content[i-1]))
 
